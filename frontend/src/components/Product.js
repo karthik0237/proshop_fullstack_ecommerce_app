a60a09b60b905cb30e71db7c3c5e08d686d75dc2
karthik kolluru <karthikkolluru123@gmail.com>
2025-06-13 19:48:17 +0530

    add: create redux store for productslist and productdetails

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import Rating from './Rating'
+import Rating from "./Rating";
 
 function Product({ product }) {
   //destructuring props
@@ -14,19 +14,25 @@ function Product({ product }) {
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-            <Card.Title as="div">
-                <strong>{product.name}</strong>
-            </Card.Title>
-            </Link>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
 
-        <Card.Text as='div'>
-            <div className="my-3">
-                <Rating value={product.rating} text={`${product.numReviews} reviews`} color='#ffe234' />
-            </div>
+        <Card.Text as="div">
+          <div className="my-3">
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+              color="#ffe234"
+            />
+          </div>
         </Card.Text>
 
-        <Card.Text as='h5'>
-            <div className="my-3"><i className="fa fa-indian-rupee-sign"></i>.{product.price}</div>
+        <Card.Text as="h5">
+          <div className="my-3">
+            <i className="fa fa-indian-rupee-sign"></i>.{product.price}
+          </div>
         </Card.Text>
       </Card.Body>
     </Card>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,74 +1,123 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
+import { React, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
-import Rating from '../components/Rating'
-import products from '../products'
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  ListGroupItem,
+} from "react-bootstrap";
 
+import Rating from "../components/Rating";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 
+import { getProductDetails } from "../redux/slice/productDetailsSlice";
+import { useDispatch, useSelector } from "react-redux";
+//import products from "../products";
 
 function ProductScreen() {
+  const params = useParams();
+  //const product = products.find((product) => product._id == params.id)
 
-  /* getting 'id' from url route 
-  <Route path='/product/:id' element={<ProductScreen />} /> in App.js */ 
-  const {id} = useParams();
+  const dispatch = useDispatch();
+  const { isLoading, product, error } = useSelector(
+    (state) => state.productDetails
+  );
+  console.log("productdetails:", product);
 
-  const product = products.find( (p) => p._id == id)
-  /**/ 
+  useEffect(() => {
+    function getprods(id) {
+      dispatch(getProductDetails(id));
+    }
+    getprods(params.id);
+  }, [dispatch]);
+
+  //const product = products.find((prod) => prod._id == params.id)
+
   return (
     <div>
-      <Link to='/' className='btn btn-light my-3'>Go Back</Link>
-      <Row>
-      <Col md={6}>
-        <Image src={product.image} alt={product.name} fluid/>
-      </Col>
+      <Link to="/" className="btn btn-light my-3">
+        Go Back
+      </Link>
 
-      <Col md={3}>
-          <ListGroup variant='flush'>
-            <ListGroupItem>
-              <h3>{product.name}</h3>
-              </ListGroupItem>
-              <ListGroupItem>
-                <Rating value={product.rating} text={`${product.numReviews} reviews`} color='#ffe234' />
-              </ListGroupItem>
-            
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error.message}</Message> // content between tags is passed as children in props to the component
+      ) : (
+        product && (
+          <Row>
+            <Col md={6}>
+              <Image
+                src={"http://127.0.0.1:8000/images/airpods.jpg"}
+                alt={product.name}
+                fluid
+              />
+            </Col>
 
-            <ListGroupItem>
-              <p>Description: {product.description}</p>
-              </ListGroupItem>
-          </ListGroup>
-      </Col>
+            <Col md={3}>
+              <ListGroup variant="flush">
+                <ListGroupItem>
+                  <h3>{product.name}</h3>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                    color="#ffe234"
+                  />
+                </ListGroupItem>
 
-      <Col md={3}>
-        <Card>
+                <ListGroupItem>
+                  <p>Description: {product.description}</p>
+                </ListGroupItem>
+              </ListGroup>
+            </Col>
 
-          <ListGroup variant='flush'>
-            <ListGroupItem>
-              <Row>
-                <Col>Price: </Col>
-                <Col><i className='fas fa-indian-rupee-sign'></i> {product.price}</Col>
-              </Row>
-            </ListGroupItem>
+            <Col md={3}>
+              <Card>
+                <ListGroup variant="flush">
+                  <ListGroupItem>
+                    <Row>
+                      <Col>Price: </Col>
+                      <Col>
+                        <i className="fas fa-indian-rupee-sign"></i>{" "}
+                        {product.price}
+                      </Col>
+                    </Row>
+                  </ListGroupItem>
 
-             <ListGroupItem>
-              <Row>
-                <Col>Status: </Col>
-                <Col>{product.countInStock > 0? 'In Stock':'Out of Stock'}</Col>
-              </Row>
-            </ListGroupItem>
+                  <ListGroupItem>
+                    <Row>
+                      <Col>Status: </Col>
+                      <Col>
+                        {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                      </Col>
+                    </Row>
+                  </ListGroupItem>
 
-            <ListGroupItem>
-              <Row>
-              <Button className='btn btn-dark' disabled={product.countInStock == 0} >Add to Cart</Button>
-              </Row>
-            </ListGroupItem>
-          </ListGroup>
-
-        </Card>
-      </Col>
-      </Row>
+                  <ListGroupItem>
+                    <Row>
+                      <Button
+                        className="btn btn-dark"
+                        disabled={product.countInStock == 0}
+                      >
+                        Add to Cart
+                      </Button>
+                    </Row>
+                  </ListGroupItem>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        )
+      )}
     </div>
-  )
+  );
 }
 
-export default ProductScreen
+export default ProductScreen;
